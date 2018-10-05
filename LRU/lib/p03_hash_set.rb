8a23825b187d35e3847e526d808986601a55b33b
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-05 15:37:25 -0400

    Pass specs for Phase III

--- LRU/lib/p03_hash_set.rb
+++ LRU/lib/p03_hash_set.rb
@@ -1,24 +1,47 @@
 class HashSet
   attr_reader :count
 
-  def initialize(num_buckets = 8)
+  def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
-  def insert(key)
+  def insert(num)
+    unless full?
+      unless self[num].include?(num)
+        self[num] << num
+        @count += 1
+      end
+    else
+      # debugger
+      resize!
+      insert(num)
+      # self[num] << num
+      # @count += 1
+    end
   end
 
-  def include?(key)
+  def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
-  def remove(key)
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  def full?
+    count >= num_buckets
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    i = num.hash % num_buckets
+    @store[i]
   end
 
   def num_buckets
@@ -26,5 +49,12 @@ class HashSet
   end
 
   def resize!
+    old_store = @store.dup
+    old_store.flatten!
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    until old_store.empty?
+      insert(old_store.shift)
+    end
   end
 end
