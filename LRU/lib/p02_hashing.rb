05ee42e35c38695ec0c25659613ef268ecb83fb1
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-05 15:29:27 -0400

    Pass specs for Phase 2

--- LRU/lib/p02_hashing.rb
+++ LRU/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    acc = 0
+    self.each_with_index do |el, idx|
+      acc += (el.hash + idx.hash) ^ acc
+    end
+    acc
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +22,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.sort_by { |k, v| v }.hash
   end
 end
