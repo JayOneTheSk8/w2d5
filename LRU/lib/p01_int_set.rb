657448615058af7e3929cfa77c347591f82f4749
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-05 14:15:11 -0400

    Pass specs phase1

--- LRU/lib/p01_int_set.rb
+++ LRU/lib/p01_int_set.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class MaxIntSet
   def initialize(max)
     @max = max
@@ -5,7 +7,7 @@ class MaxIntSet
   end
 
   def insert(num)
-    raise "Out of bounds" if is_valid?(num)
+    raise "Out of bounds" unless is_valid?(num)
     @store[num] = true
   end
 
@@ -20,7 +22,7 @@ class MaxIntSet
   private
 
   def is_valid?(num)
-    num > @max || num < 0
+    num < @max && num > 0
   end
 
   def validate!(num)
@@ -66,18 +68,41 @@ class ResizingIntSet
   end
 
   def insert(num)
+    unless full?
+      unless self[num].include?(num)
+        self[num] << num
+        @count += 1
+      end
+    else
+      # debugger
+      resize!
+      insert(num)
+      # self[num] << num
+      # @count += 1
+    end
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
+  end
+
+  def full?
+    count >= num_buckets
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    i = num % num_buckets
+    @store[i]
   end
 
   def num_buckets
@@ -85,5 +110,12 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store = @store.dup
+    old_store.flatten!
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    until old_store.empty?
+      insert(old_store.shift)
+    end
   end
 end
